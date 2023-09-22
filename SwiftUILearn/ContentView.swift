0ab6f21f16b263eba0197f32da07de001bb2bdb2
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 14:13:34 +0500

    NavigationStack, but some stuff doest work correctly

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -9,28 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Button(action: {
-            print("Button pressed")
-        }, label: {
-            Text("Press me!")
-                .font(.largeTitle)
-                .foregroundColor(.white)
-        })
-        .padding()
-        .background(
-            LinearGradient(gradient: Gradient(colors: [.purple, .pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
-        )
-        .cornerRadius(10)
+        NavigationStack {
+            VStack{
+                Text("Welcome to my app!")
+                NavigationLink(destination: DetailView()) {
+                    Text("Go to Detail View")
+                }
+            }
+        }
+        .navigationTitle("Home")
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button(action: {
+                    print("Settings tapped")
+                }, label: {
+                    Text("Settings")
+                })
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        Text("This is the detail view!")
+            .navigationTitle("Detail")
+            .navigationBarTitleDisplayMode(.large)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewDevice("iPhone 14 Pro")
-        ContentView()
-            .preferredColorScheme(.dark)
-        ContentView()
-            .previewInterfaceOrientation(.landscapeLeft)
     }
 }
