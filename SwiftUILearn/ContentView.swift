a6b0b9d6863fb5495d44d88f1e81d74db6361913
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 17:20:46 +0500

    ClipShapes

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -13,17 +13,12 @@ struct ContentView: View {
     @State private var password = ""
     
     var body: some View {
-        VStack{
-            Text("Hello, shadows!")
-                .font(.largeTitle)
-                .padding()
-                .background(.white)
-                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-            Circle()
-                .fill(.blue)
-                .shadow(color: .purple, radius: 10, x: 0, y: 0)
-                .padding(EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10))
-        }
+        Text("Hello, ClipShape!")
+            .font(.largeTitle)
+            .padding()
+            .background(Color.blue)
+            .foregroundStyle(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
     }
 }
 
--- SwiftUILearn/MyView.swift
+++ SwiftUILearn/MyView.swift
@@ -9,9 +9,48 @@ import SwiftUI
 
 struct MyView: View {
     var body: some View {
-        Text("Welcome to my amazing app!")
-            .font(.title)
-            .foregroundStyle(.blue)
+        VStack {
+            Text("Circle")
+                .frame(width: 200, height: 200)
+                .font(.title)
+                .padding()
+                .background(.orange)
+                .foregroundStyle(.white)
+                .clipShape(Circle())
+            Text("Ellipse")
+                .frame(width: 200, height: 100)
+                .font(.title)
+                .padding()
+                .background(.orange)
+                .foregroundStyle(.white)
+                .clipShape(Ellipse())
+            Text("Capsule")
+                .frame(width: 200, height: 100)
+                .font(.title)
+                .padding()
+                .background(.orange)
+                .foregroundStyle(.white)
+                .clipShape(Capsule())
+            Text("Custom")
+                .frame(width: 200, height: 100)
+                .font(.title)
+                .padding()
+                .background(.orange)
+                .foregroundStyle(.black)
+                .clipShape(CustomShape())
+        }
+    }
+}
+
+struct CustomShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.closeSubpath()
+        
+        return path
     }
 }
 
