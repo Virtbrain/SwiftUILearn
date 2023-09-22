68aecdc0d0bbee434986fffb7cc75d785cbce952
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 19:26:53 +0500

    View Modifier

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -8,25 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5","Item 6", "Item 7", "Item 8", "Item 9"]
-    let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible()),
-        GridItem(.flexible())
-    ]
     
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 16) {
-            ForEach(items, id: \.self) { item in
-                Text(item)
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 100)
-                    .background(.green)
-                    .foregroundStyle(.white)
-                    .clipShape(.buttonBorder)
-            }
-        }
-        .padding()
+        Text("Hello, World!")
+            .modifier(BoldAndItalicModifier())
+    }
+}
+
+struct BoldAndItalicModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .fontWeight(.bold)
+            .italic()
     }
 }
 
