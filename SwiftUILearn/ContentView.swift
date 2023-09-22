4028e2ef159ff258301bf5fe209454557035d038
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 20:17:07 +0500

    Free shape

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -10,16 +10,25 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        Text("Hello, World!")
-            .modifier(BoldAndItalicModifier())
+        DiamondShape()
+            .fill(.red)
+            .frame(width: 200, height: 200)
     }
 }
-
-struct BoldAndItalicModifier: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .fontWeight(.bold)
-            .italic()
+    
+struct DiamondShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let center = CGPoint(x: rect.width / 2, y: rect.height / 2)
+        
+        path.move(to: CGPoint(x: center.x, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: center.y))
+        path.addLine(to: CGPoint(x: center.x, y: rect.height))
+        path.addLine(to: CGPoint(x: 0, y: center.y))
+        path.addLine(to: CGPoint(x: center.x, y: 0))
+        
+        return path
     }
 }
 
