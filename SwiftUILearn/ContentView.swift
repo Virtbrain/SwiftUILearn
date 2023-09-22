983e0fadddf3947f0bc16631f6066dbf997295b9
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 18:04:12 +0500

    Lazy V/H Grid

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -8,17 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    let items = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5","Item 6", "Item 7", "Item 8", "Item 9"]
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     
     var body: some View {
-        Text("Hello, \nCircular View!")
-            .multilineTextAlignment(.center)
-            .padding(50)
-            .foregroundStyle(.white)
-            .background(
-                Circle()
-                    .fill(.blue)
-                    .frame(width: 200, height: 200)
-            )
+        LazyVGrid(columns: columns, spacing: 16) {
+            ForEach(items, id: \.self) { item in
+                Text(item)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .background(.green)
+                    .foregroundStyle(.white)
+                    .clipShape(.buttonBorder)
+            }
+        }
+        .padding()
     }
 }
 
