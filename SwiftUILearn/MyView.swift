d87f80492ca534e1c1297b6f6e6bd5769be05266
AlexeyManankov <virtbrain@gmail.com>
2023-09-22 16:12:11 +0500

    newFiles

---
+++ SwiftUILearn/MyView.swift
@@ -0,0 +1,20 @@
+//
+//  MyView.swift
+//  SwiftUILearn
+//
+//  Created by Alexey Manankov on 22.09.2023.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text("Welcome to my amazing app!")
+            .font(.title)
+            .foregroundStyle(.blue)
+    }
+}
+
+#Preview {
+    MyView()
+}
