c54ec4cae4ac8dcb01aa2c3ad0d98529436b3cee
AlexeyManankov <virtbrain@gmail.com>
2023-09-23 21:30:03 +0500

    Enviroment(crappy shit)

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,27 +7,68 @@
 
 import SwiftUI
 
+enum Theme {
+    case light, dark
+}
 
-struct ContentView: View {
-    @AppStorage("username") var username: String = "Anonymus"
+struct ThemeKey: EnvironmentKey {
+    static let defaultValue: Theme = .light
+}
+
+extension EnvironmentValues {
+    var theme: Theme {
+        get { self[ThemeKey.self] }
+        set { self[ThemeKey.self] = newValue }
+    }
+}
+
+struct ThemedView: View {
+    @Environment(\.theme) var theme: Theme
     
     var body: some View {
         VStack {
-            Text("Welcome, \(username)!")
-                .padding()
-                .font(.title)
-            
-            Button(action: {
-                print(username)
-            }, label: {
-                Text("Log in")
-                    .padding()
-            })
-            .background(.blue)
-            .font(.title)
-            .foregroundStyle(.white)
-            .clipShape(.buttonBorder)
+            if theme == .light {
+                Text("Light Theme")
+                    .foregroundStyle(.black)
+                    .background(.white)
+            } else {
+                Text("Dark Theme")
+                    .foregroundStyle(.white)
+                    .background(.black)
+            }
         }
+        .padding()
+    }
+}
+
+
+struct ContentView: View {
+    @State var theme: Theme = .light
+    
+    var body: some View {
+        VStack {
+            Button("Switch Theme") {
+                switch theme {
+                case .light:
+                    theme = .dark
+                case .dark:
+                    theme = .light
+                }
+            }
+            .background(.blue)
+            .padding()
+            .foregroundStyle(.white)
+            .font(.title)
+            
+            ThemedView()
+        }
+        .theme(theme)
+    }
+}
+
+extension View {
+    func theme(_ theme: Theme) -> some View {
+        environment(\.theme, theme)
     }
 }
 
