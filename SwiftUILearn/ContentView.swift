a09fbecac82c8019012a8ac37625bc53a01d46b4
AlexeyManankov <virtbrain@gmail.com>
2023-09-23 20:55:41 +0500

    SceneStorage

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -6,54 +6,28 @@
 //
 
 import SwiftUI
-import Combine
+
 
 struct ContentView: View {
-    @StateObject private var jokeFetcher = JokeFetcher()
+    @AppStorage("username") var username: String = "Anonymus"
     
     var body: some View {
         VStack {
-            Text(jokeFetcher.joke)
+            Text("Welcome, \(username)!")
                 .padding()
+                .font(.title)
+            
             Button(action: {
-                jokeFetcher.fetchJoke()
+                print(username)
             }, label: {
-                Text("Fetch Joke")
+                Text("Log in")
                     .padding()
             })
-            .background(.green)
-            .foregroundStyle(.white)
+            .background(.blue)
             .font(.title)
+            .foregroundStyle(.white)
             .clipShape(.buttonBorder)
         }
-        .onAppear {
-            jokeFetcher.fetchJoke()
-        }
-    }
-}
-
-class JokeFetcher: ObservableObject {
-    @Published var joke: String = ""
-    
-    private var cancellable: AnyCancellable?
-    private let jokes = [
-        "Why don't scientists trust atoms? Because they make up everything!",
-        "Why did the bicycle fall over? Because it was two tired!",
-        "Why don't some animals play cards? Because they are afraid of cheetahs!",
-        "Why did the scarecrow win an award? Because he was outstanding in his field!"
-    ]
-    
-    func fetchJoke() {
-        //Networking code here
-        //Update `joke` with the result
-        
-        if let randomJoke = jokes.randomElement() {
-            joke = randomJoke
-        }
-     }
-    
-    deinit {
-        cancellable?.cancel()
     }
 }
 
--- SwiftUILearn/MyView.swift
+++ SwiftUILearn/MyView.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
-struct GameView: View {
+struct SwiftUIView: View {
+    @SceneStorage("selectedTab") var selectedTab: String?
     
     var body: some View {
-        VStack {
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .tabItem {
+                    Label("Tab 1", systemImage: "1.circle")
+                }
+                .tag("Tab1")
             
+            Text("Tab 2")
+                .tabItem {
+                    Label("Tab 2", systemImage: "2.circle")
+                }
+                .tag("Tab2")
         }
     }
 }
 
+#Preview {
+    SwiftUIView()
+}
