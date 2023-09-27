69673a3c7106365884f79d659f57ec6ae2a132e2
AlexeyManankov <virtbrain@gmail.com>
2023-09-27 15:22:34 +0500

    Text in swiftUI

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,46 +7,11 @@
 
 import SwiftUI
 
-class TaskManager: ObservableObject {
-    @Published var tasks = [String]()
-    
-    func addTask(_ task: String) {
-        tasks.append(task)
-    }
-}
-
-struct TaskListView: View {
-    @EnvironmentObject var taskManager: TaskManager
-    @State private var newTask = ""
-    
-    var body: some View {
-        NavigationStack {
-            VStack {
-                TextField("New task", text: $newTask)
-                    .onSubmit {
-                        if !newTask.isEmpty {
-                            taskManager.addTask(newTask)
-                            newTask = ""
-                        }
-                    }
-                    .padding()
-                List(taskManager.tasks, id:\.self) { task in
-                    Text(task)
-                }
-            }
-            .navigationTitle("Task List")
-        }
-    }
-}
-
-
-
 struct ContentView: View {
-    @StateObject var taskManager = TaskManager()
     
     var body: some View {
-        TaskListView()
-            .environmentObject(taskManager)
+        Text("Hello, world!")
+            .font(.title)
     }
 }
 
