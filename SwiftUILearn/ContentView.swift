7bb0eba68cdcbef3ea4c403b5387f00fc57cc254
AlexeyManankov <virtbrain@gmail.com>
2023-09-28 17:38:06 +0500

    secure field

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,31 +7,16 @@
 
 import SwiftUI
 
-let quote = """
-**"Be yourself;** everyone else is _already taken._"
-- **Oscar Wilde**
-"""
-
-let attributedQuote = try! AttributedString(markdown: quote)
-
 struct ContentView: View {
+    @State private var password = ""
     var body: some View {
-        VStack{
-            Text("Hello, world!")
-                .fontWeight(.semibold)
-                .italic()
-                .underline()
-                .strikethrough(true, color: .red)
-                .foregroundStyle(Color(red: 69/255, green: 217/255, blue: 79/255))
-                .padding()
-            
-            Text("Markdown in the text like **Bold** and _italic_")
-                .foregroundStyle(.blue)
-            
-            Text(attributedQuote)
-                .font(.system(size: 16, weight: .medium, design: .serif))
-                .foregroundStyle(.red)
-        }
+        SecureField("Enter your password", text: $password)
+            .textContentType(.password)
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 5).stroke())
+            .multilineTextAlignment(.center)
+            .padding()
+
     }
 }
 
