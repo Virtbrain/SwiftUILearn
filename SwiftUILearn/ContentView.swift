31c60124950af8a1e559a7e25e0d7da6da096b6e
AlexeyManankov <virtbrain@gmail.com>
2023-10-11 17:43:27 +0500

    Buttons

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -10,53 +10,15 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var isAnimating = false
-    let image: Image
-    
-    init() {
-        let uiImage = UIImage(named: "CaneCorso") ?? UIImage()
-        let filteredUIImage = ContentView.applySepiaFilter(to: uiImage)
-        image = Image(uiImage: filteredUIImage)
-    }
-    
     var body: some View {
-        image
-            .resizable()
-            .scaledToFit()
-            .shadow(color: Color.gray, radius:20, x: 15, y: 15)
-            .border(Color.black, width: 1)
-            .padding()
-            .scaleEffect(isAnimating ? 1.5 : 1.0)
-            .onAppear() {
-                withAnimation(.easeInOut(duration: 1.0).repeatForever(autoreverses: true)) {
-                    isAnimating = true
-                }
-            }
-            
-    }
-    
-    static func applySepiaFilter(to inputImage: UIImage) -> UIImage {
-        guard let ciImage = CIImage(image: inputImage) else {
-            return inputImage
+        Button("Click me!") {
+            //Action
         }
-        
-        let filter = CIFilter.sepiaTone()
-        filter.inputImage = ciImage
-        filter.intensity = 1.0
-        
-        //Get a CIContext
-        let context = CIContext()
-        
-        //create a CGImage form CIImage
-        guard let outputCIImage = filter.outputImage,
-              let cgImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else {
-            return inputImage
-        }
-        
-        //Create UIImage from the CGImage
-        let outputUIImage = UIImage(cgImage: cgImage)
-        
-        return outputUIImage
+        .font(.headline)
+        .padding()
+        .foregroundStyle(.white)
+        .background(.blue)
+        .cornerRadius(5.0)
     }
 }
     
