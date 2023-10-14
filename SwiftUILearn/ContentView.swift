d77dbadd2340f5f45cb871ff842a928ecedea068
AlexeyManankov <virtbrain@gmail.com>
2023-10-14 22:03:23 +0500

    navigation stack

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,16 +7,53 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    let names = ["Alice", "Bob", "Charlie", "Dave"]
+struct Park: Hashable {
+    let name: String
+    let imageName: String
+    let description: String
+}
+
+extension Park: Identifiable {
+    var id: String { name }
+}
+
+struct ParkDetailsView: View {
+    let park: Park
     
     var body: some View {
-        NavigationStack{
-            List(names, id: \.self) { name in
-               Text(name)
-                    .listRowBackground(
-                        LinearGradient(gradient: Gradient(colors: [.blue, .purple]), startPoint: .leading, endPoint: .trailing)
-                        )
+        VStack {
+            Image(park.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200, height: 200)
+            Text(park.name)
+                .font(.title)
+                .foregroundStyle(Color.primary)
+            Text(park.description)
+                .font(.body)
+                .foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var presentedParks: [Park] = []
+    
+    var parks: [Park] {
+        [
+            Park(name: "Yosemite", imageName: "yosemite", description: "Yosemite National Park"),
+            Park(name: "Sequoia", imageName: "sequoia", description: "Sequoia Natiaonal Park"),
+            Park(name: "Point Reyes", imageName: "point_reyes", description: "Point Reyes National Seashore")
+        ]
+    }
+    var body: some View {
+        NavigationStack(path: $presentedParks) {
+            List(parks) { park in
+                NavigationLink(park.name, value: park)
+            }
+            .navigationDestination(for: Park.self) { park in
+                ParkDetailsView(park: park)
             }
         }
     }
