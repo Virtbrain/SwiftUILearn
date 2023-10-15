101e4fab18f66d82eee7b28ba0c552858d9a6ea4
AlexeyManankov <virtbrain@gmail.com>
2023-10-15 21:27:21 +0500

    Scrolling list

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -8,25 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    var animals = ["Lion", "Tiger", "Elephant", "Leopard"]
-    var plants = ["rose", "Lily", "Tulip", "Orchid"]
+    @State private var numbers: [Int] = Array(1...20)
+    @State private var isLoading = false
+    @State private var isFinished = false
     
     var body: some View {
-        TabView {
-            List(animals, id: \.self) { animal in
-                Text(animal)
+        NavigationStack {
+            List {
+                ForEach(numbers, id: \.self) { number in
+                    Text("Row \(number)")
+                }
+                
+                if !isFinished {
+                    ProgressView()
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .foregroundStyle(.black)
+                        .foregroundStyle(.red)
+                        .onAppear {
+                            loadMoreContent()
+                        }
+                }
             }
-            .tabItem {
-                Image(systemName: "hare")
-                Text("Animals")
-            }
-            
-            List(plants, id: \.self) { plant in
-                Text(plant)
-            }
-            .tabItem {
-                Image(systemName: "leaf")
-                Text("Plants")
+            .navigationTitle("Infinite List")
+        }
+    }
+    
+    func loadMoreContent() {
+        if !isLoading {
+            isLoading = true
+            //This simulates an asyncronus call
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                let moreNumbers = numbers.count + 1...numbers.count + 20
+                numbers.append(contentsOf: moreNumbers)
+                isLoading = false
+                if numbers.count > 250 {
+                    isFinished = true
+                }
             }
         }
     }
