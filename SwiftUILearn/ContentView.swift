b3befbe47cb1123944f2e46ac5b2d5449ba514c3
AlexeyManankov <virtbrain@gmail.com>
2023-10-19 22:20:15 +0500

    Hide TabView

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var selectedTab:Int = 1
+    @State private var isTabViewHidden = false
     
     var body: some View {
         VStack {
@@ -17,17 +18,26 @@ struct ContentView: View {
             }
             .padding()
             
-            TabView(selection: $selectedTab) {
-                Text("First Tab")
-                    .tabItem {
-                        Image(systemName: "1.circle")
-                        Text("Tab 1")
-                    }.tag(1)
-                Text("Second Tab")
-                    .tabItem {
-                        Image(systemName: "2.circle")
-                        Text("Tab 2")
-                    }.tag(2)
+            Button(action: {
+                isTabViewHidden.toggle()
+            }, label: {
+                Text(isTabViewHidden ? "Show Tab View" : "Hide Tab View")
+            })
+            
+            if !isTabViewHidden {
+                TabView(selection: $selectedTab) {
+                    Text("First Tab")
+                        .tabItem {
+                            Image(systemName: "1.circle")
+                            Text("Tab 1")
+                        }.tag(1)
+                    Text("Second Tab")
+                        .tabItem {
+                            Image(systemName: "2.circle")
+                            Text("Tab 2")
+                        }.tag(2)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
         }
     }
