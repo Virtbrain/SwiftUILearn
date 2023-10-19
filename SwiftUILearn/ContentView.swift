1ad0e0bedc08e846cd7a20ec86f48203cbd783d7
AlexeyManankov <virtbrain@gmail.com>
2023-10-19 22:16:18 +0500

    Switch tabs programmaticly

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,40 +7,28 @@
 
 import SwiftUI
 
-struct ModalView: View {
-    @Binding var isPresented: Bool
-    
-    var body: some View {
-        Text("this is a modal view.")
-            .padding()
-        Button("Dissmiss") {
-            isPresented = false
-        }
-    }
-}
-
 struct ContentView: View {
-    @State private var isPresented = false
     @State private var selectedTab:Int = 1
     
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("First Tab")
-                .tabItem {
-                    Image(systemName: "1.circle")
-                    Text("Tab 1")
-                }.tag(1)
-            Text("Second Tab")
-                .tabItem {
-                    Image(systemName: "2.circle")
-                    Text("Tab 2")
-                }.tag(2)
-        }
-        .onChange(of: selectedTab) { _, _ in
-            isPresented = true
-        }
-        .sheet(isPresented: $isPresented) {
-            ModalView(isPresented: $isPresented)
+        VStack {
+            Button("Switch to second tab") {
+                selectedTab = 1
+            }
+            .padding()
+            
+            TabView(selection: $selectedTab) {
+                Text("First Tab")
+                    .tabItem {
+                        Image(systemName: "1.circle")
+                        Text("Tab 1")
+                    }.tag(1)
+                Text("Second Tab")
+                    .tabItem {
+                        Image(systemName: "2.circle")
+                        Text("Tab 2")
+                    }.tag(2)
+            }
         }
     }
 }
