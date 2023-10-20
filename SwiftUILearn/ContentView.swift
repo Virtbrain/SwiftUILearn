3c4aedfc724add7bc1f9a5e571c86f716b79e2c8
AlexeyManankov <virtbrain@gmail.com>
2023-10-20 16:36:26 +0500

    Circular prorgress view

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -6,32 +6,26 @@
 //
 
 import SwiftUI
-import Combine
+
+struct SpinnerView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(.circular)
+            .tint(.blue)
+            .scaleEffect(2.0, anchor: .center)
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    //Simulates delay
+                    //perform transitition to the next view here
+                }
+            }
+    }
+}
 
 struct ContentView: View {
-    @State private var progress: CGFloat = 0.0
-    
-    let timer = Timer.publish(every: 0.1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        ZStack(alignment: .leading, content: {
-            Rectangle()
-                .frame(width: 300, height: 20)
-                .opacity(0.3)
-                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
-                .foregroundStyle(Color.gray)
-            
-            Rectangle()
-                .frame(width: progress * 300, height: 20)
-                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
-                .foregroundStyle(Color.green)
-                .animation(.easeInOut, value: progress)
-        })
-        .onReceive(timer) {_ in
-            if progress < 1.0 {
-                progress += 0.01
-            }
-        }
+        SpinnerView()
     }
 }
 
