37360fdd7cbe6053f8e50029319e399b9d37546b
AlexeyManankov <virtbrain@gmail.com>
2023-10-20 17:17:53 +0500

    linear ProgressView

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,25 +7,70 @@
 
 import SwiftUI
 
-struct SpinnerView: View {
+enum Operations: String {
+    case plus = "+"
+    case minus = "-"
+}
+
+struct MathButton: View {
+    let operation: Operations
+    let valueOperation: Double
+    let competion: (Operations, Double) -> Void
+
     var body: some View {
-        ProgressView()
-            .progressViewStyle(.circular)
-            .tint(.blue)
-            .scaleEffect(2.0, anchor: .center)
-            .onAppear() {
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-                    //Simulates delay
-                    //perform transitition to the next view here
-                }
-            }
+        Button(action: {
+            competion(operation, valueOperation)
+        }, label: {
+            Text("\(operation.rawValue) 0.1")
+                .bold()
+                .padding()
+                .font(.title)
+        })
+        .background(Color.orange)
+        .foregroundStyle(Color.white)
+        .clipShape(RoundedRectangle(cornerRadius: 12.0))
+        .shadow(color: .black.opacity(0.3), radius: 5, x: 3, y: 5)
+        .padding()
     }
 }
 
 struct ContentView: View {
+    @State private var progress = 0.5
+    
+    func mathOperation(operation: Operations, value: Double) {
+        switch operation {
+        case .plus:
+            if progress < 0.9 {
+                progress += value
+            } else {
+                progress = 1.0
+            }
+        case .minus:
+            if progress > 0.1 {
+                progress -= value
+            } else {
+                progress = 0.0
+            }
+        }
+    }
     
     var body: some View {
-        SpinnerView()
+        VStack {
+            ProgressView(value: progress, total: 1.0)
+                .progressViewStyle(.linear)
+                .padding()
+                
+            HStack {
+                MathButton(operation: .plus, valueOperation: 0.1, competion: mathOperation)
+                
+                Spacer()
+                
+                MathButton(operation: .minus, valueOperation: 0.1, competion: mathOperation)
+                
+            }
+            
+            Text("The progress now is: \(String(format: "%.2f", progress))")
+        }
     }
 }
 
