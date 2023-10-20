edc5217aa651f5a0ed8bbc167b60b165968a0d11
AlexeyManankov <virtbrain@gmail.com>
2023-10-20 19:05:06 +0500

    ProgressView in NavigationStack

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,32 +7,19 @@
 
 import SwiftUI
 
-struct IndeteminateProgressView: View {
-    @State private var isLoading = true
-    
-    var body: some View {
-        VStack {
-            ProgressView("Loading...")
-                .progressViewStyle(.circular)
-                .scaleEffect(2.0, anchor: .center)
-        }
-        .onAppear {
-            startAnimating()
-        }
-    }
-    
-    func startAnimating() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            withAnimation(Animation.linear(duration: 1.5).repeatForever()) {
-                isLoading = true
-            }
-        }
-    }
-}
-
 struct ContentView: View {
     var body: some View {
-        IndeteminateProgressView()
+        NavigationStack {
+            VStack {
+                Text("ProgressView in the NavigationStrack!")
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    ProgressView()
+                        .tint(Color.green)
+                }
+            }
+        }
     }
 }
 
