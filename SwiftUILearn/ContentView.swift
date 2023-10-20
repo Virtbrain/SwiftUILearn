14edf5b889748da5cf6dd258d634a84c7f415cc0
AlexeyManankov <virtbrain@gmail.com>
2023-10-20 19:01:10 +0500

    indeterminate progress

--- SwiftUILearn/ContentView.swift
+++ SwiftUILearn/ContentView.swift
@@ -7,36 +7,32 @@
 
 import SwiftUI
 
-struct CircularProgress: View {
-    let progress: CGFloat
+struct IndeteminateProgressView: View {
+    @State private var isLoading = true
     
     var body: some View {
-        ZStack {
-            Circle()
-                .stroke(lineWidth: 20)
-                .opacity(0.1)
-                .foregroundStyle(Color.blue)
-            Circle()
-                .trim(from: 0.0, to: min(progress, 1.0))
-                .stroke(style: StrokeStyle(lineWidth: 20, lineCap: .round, lineJoin: .round))
-                .foregroundStyle(Color.blue)
-                .rotationEffect(Angle(degrees: 270.0))
-                .animation(.linear, value: progress)
+        VStack {
+            ProgressView("Loading...")
+                .progressViewStyle(.circular)
+                .scaleEffect(2.0, anchor: .center)
+        }
+        .onAppear {
+            startAnimating()
+        }
+    }
+    
+    func startAnimating() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation(Animation.linear(duration: 1.5).repeatForever()) {
+                isLoading = true
+            }
         }
     }
 }
 
 struct ContentView: View {
-    @State private var progress: CGFloat = 0.2
-    
     var body: some View {
-        VStack {
-            CircularProgress(progress: progress)
-                .frame(width: 200, height: 200)
-            
-            Slider(value: $progress, in: 0...1)
-                .padding()
-        }
+        IndeteminateProgressView()
     }
 }
 
